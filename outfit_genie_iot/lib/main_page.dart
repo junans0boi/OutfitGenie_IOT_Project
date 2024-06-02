9e62ed69b68394e2b07c0d3988b87ce1c97bbc4c
junans0boi <105141262+junans0boi@users.noreply.github.com>
2024-06-02 18:54:54 +0900

    ADD Mac Ver GPS

--- outfit_genie_iot/lib/main_page.dart
+++ outfit_genie_iot/lib/main_page.dart
@@ -1,8 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'add_clothes_page.dart';
 import 'profile_page.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  String _locationMessage = "Location not available";
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // 위치 서비스 활성화 확인
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      setState(() {
+        _locationMessage = "Location services are disabled.";
+      });
+      return;
+    }
+
+    // 위치 권한 확인
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        setState(() {
+          _locationMessage = "Location permissions are denied";
+        });
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      setState(() {
+        _locationMessage =
+        "Location permissions are permanently denied, we cannot request permissions.";
+      });
+      return;
+    }
+
+    // 현재 위치 가져오기
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _locationMessage = "Latitude: ${position.latitude}, Longitude: ${position.longitude}";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,13 +99,10 @@ class MainPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // 날씨 정보 표시
-            Text('Weather info will be displayed here'),
+            Text(_locationMessage),
             ElevatedButton(
-              onPressed: () {
-                // GPS로 날씨 정보 가져오기
-              },
-              child: Text('Fetch Weather Info'),
+              onPressed: _getCurrentLocation,
+              child: Text('Fetch Location Info'),
             ),
           ],
         ),
